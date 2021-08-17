97bb9b1a65f755d9d3550e23cd08eacbd7102358
lee sherwood <leesherwood28@googlemail.com>
2021-08-17 18:00:49 +0100

    Adding carasouel

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ class MyDocument extends Document {
         <Html>
           <Head>
             <link rel="icon" href="/favicon.ico" />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"></link>
           </Head>
           <body>
             <Main />
