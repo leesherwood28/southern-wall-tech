a108d1d9b0c0c05f042269e9d569ef2f785b1ae2
lee sherwood <leesherwood28@googlemail.com>
2021-08-17 14:30:44 +0100

    Refactoring sidenav

--- components/header.js
+++ components/header.js
@@ -1,22 +1,26 @@
 import ContactButton from './contact-button'
 import HeaderLink from './header-link'
 import Menu from './menu';
+import SideNavLink from './side-nav-link';
 
 export default function Header() {
+
+  const links = [
+      {text: "Home", href: "/"},
+      {text: "About", href: "/About"},
+      {text: "Gallery", href: "/Gallery"},
+    ]
+
   return (
         <header className="bg-black h-12 flex items-stretch">
             <div className="flex items-stretch">
                 <Menu>
-                    <HeaderLink text="Home" href="/"></HeaderLink>
-                    <HeaderLink text="About" href="/"></HeaderLink>
-                    <HeaderLink text="Gallery" href="/Gallery"></HeaderLink>
+                    {links.map(link => (<SideNavLink key={link.href} text={link.text} href={link.href}></SideNavLink>))}
                 </Menu>
             </div>
             
             <div className="flex-grow flex items-stretch justify-center">
-                <HeaderLink text="Home" href="/"></HeaderLink>
-                <HeaderLink text="About" href="/"></HeaderLink>
-                <HeaderLink text="Gallery" href="/Gallery"></HeaderLink>
+                {links.map(link => (<HeaderLink key={link.href} text={link.text} href={link.href}></HeaderLink>))}
             </div>
             <div className="flex items-stretch">
                 <ContactButton></ContactButton>
---
+++ components/side-nav-link.js
@@ -0,0 +1,7 @@
+
+export default function SideNavLink({href, text}) {
+    return (
+      <a className="text-white px-8 flex items-center hover:bg-white hover:bg-opacity-20" href={href}>{text}</a>
+    )
+  }
+  
