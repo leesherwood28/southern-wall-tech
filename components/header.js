6266e7334ba1a22c60fbca5f0af308ecf6a67b5d
lee sherwood <leesherwood28@googlemail.com>
2021-08-17 13:01:24 +0100

    Improving use of link button

--- components/header.js
+++ components/header.js
@@ -1,8 +1,10 @@
 import ContactButton from './contact-button'
+import HeaderLink from './header-link'
 
 export default function Header() {
   return (
         <header className="bg-black h-12 flex items-center">
+            <HeaderLink text="Gallery" href="/Gallery"></HeaderLink>
             <ContactButton></ContactButton>
         </header>
   )
