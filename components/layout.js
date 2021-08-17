3bbcd480dd4045dae9c79c7eb2d3e354eb8a3081
lee sherwood <leesherwood28@googlemail.com>
2021-08-17 17:14:29 +0100

    Improving site layout

--- components/layout.js
+++ components/layout.js
@@ -2,9 +2,9 @@ import Header from './header'
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="h-screen w-screen flex flex-col items-stretch">
       <Header></Header>
-      <main>{children}</main>
-    </>
+      <main className="overflow-y-auto flex-grow">{children}</main>
+    </div>
   )
 }
