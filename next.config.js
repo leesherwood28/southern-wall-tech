2153744589e1a5b048dddad007338a104e406a28
lee sherwood <leesherwood28@googlemail.com>
2021-08-17 15:47:40 +0100

    Fixing up config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    if (ANALYZE) {
-     config.plugins.push(
-       new BundleAnalyzerPlugin({
-         analyzerMode: 'server',
-         analyzerPort: isServer ? 8888 : 8889,
-         openAnalyzer: true,
-       })
-     )
-    }
-    return config
-  },
+  reactStrictMode: true,
 }
