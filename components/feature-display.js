b2c8b2a3d6e82ae454d5e3156487b922c4a4c2a3
lee sherwood <leesherwood28@googlemail.com>
2021-08-17 19:28:15 +0100

    Improving site look and feel

--- components/feature-display.js
+++ components/feature-display.js
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 export default function FeatureDisplay({image, alt, text}) {
   return (
-    <div className="flex flex-col items-center w-60">
+    <div className="flex flex-col items-center w-60 p-4">
         <Image src={image} alt={alt} width={200} height={200}></Image>
-        <span className="text-2xl font-light text-center">
+        <span className="text-xl md:text-2xl font-light text-center">
             {text}
         </span>
     </div>
