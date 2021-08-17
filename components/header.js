3515b810a51d53ec7df0802a98b9880edfdcc627
lee sherwood <leesherwood28@googlemail.com>
2021-08-17 12:29:12 +0100

    Adding header

--- components/header.js
+++ components/header.js
@@ -1,12 +1,10 @@
-import styles from './layout.module.css'
+import styles from './header.module.css'
 import ContactButton from './contact-button'
 
-export default function Header({ children }) {
+export default function Header() {
   return (
-    <>
         <header className={styles.header}>
             <ContactButton></ContactButton>
         </header>
-    </>
   )
 }
