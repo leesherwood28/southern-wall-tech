a8c1589965e5d3a2fe766a0f7e9529069c8bda95
lee sherwood <leesherwood28@googlemail.com>
2021-08-17 19:13:47 +0100

    Improving feature display

--- components/feature-display.js
+++ components/feature-display.js
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 export default function FeatureDisplay({image, alt, text}) {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center w-60">
         <Image src={image} alt={alt} width={200} height={200}></Image>
-        <span className="text-2xl font-light">
+        <span className="text-2xl font-light text-center">
             {text}
         </span>
     </div>
