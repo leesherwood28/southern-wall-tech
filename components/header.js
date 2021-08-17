8b6dd9b77d5c28e1af662c3390b941a26de402a8
lee sherwood <leesherwood28@googlemail.com>
2021-08-17 13:35:24 +0100

    removing burget

--- components/header.js
+++ components/header.js
@@ -1,10 +1,17 @@
 import ContactButton from './contact-button'
 import HeaderLink from './header-link'
+import { slide as Menu } from 'react-burger-menu'
 
 export default function Header() {
   return (
         <header className="bg-black h-12 flex items-stretch">
-            <div></div>
+            <div>
+                <Menu>
+                    <HeaderLink text="Home" href="/"></HeaderLink>
+                    <HeaderLink text="About" href="/"></HeaderLink>
+                    <HeaderLink text="Gallery" href="/Gallery"></HeaderLink>
+                </Menu>
+            </div>
             
             <div className="flex-grow flex items-stretch justify-center">
                 <HeaderLink text="Home" href="/"></HeaderLink>
