5ab2a19b51f6320a7088dec83abd9a47deeda9fe
lee sherwood <leesherwood28@googlemail.com>
2021-08-17 13:04:55 +0100

    Adding header links to page

--- components/header-link.js
+++ components/header-link.js
@@ -1,7 +1,7 @@
 
 export default function HeaderLink({href, text}) {
     return (
-      <a className="text-white h-full p-2 flex items-center hover:bg-white hover:bg-opacity-20" href={href}>{text}</a>
+      <a className="text-white p-2 flex items-center hover:bg-white hover:bg-opacity-20" href={href}>{text}</a>
     )
   }
   
--- components/header.js
+++ components/header.js
@@ -3,9 +3,15 @@ import HeaderLink from './header-link'
 
 export default function Header() {
   return (
-        <header className="bg-black h-12 flex items-center">
-            <HeaderLink text="Gallery" href="/Gallery"></HeaderLink>
-            <ContactButton></ContactButton>
+        <header className="bg-black h-12 flex items-stretch">
+            <div></div>
+            
+            <div className="flex-grow flex items-stretch justify-center">
+                <HeaderLink text="Gallery" href="/Gallery"></HeaderLink>
+            </div>
+            <div className="flex items-stretch">
+                <ContactButton></ContactButton>
+            </div>
         </header>
   )
 }
