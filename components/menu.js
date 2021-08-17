c87a1160dedbb9b116807f0e9605c33c2c8d7b60
lee sherwood <leesherwood28@googlemail.com>
2021-08-17 14:21:27 +0100

    Adding menu

--- components/menu.js
+++ components/menu.js
@@ -1,7 +1,28 @@
+import { createContext, useContext, useState } from "react"
+
+const MenuContext = createContext(false);
 
 export default function Menu({children}) {
+
+    const [isOpen, setOpen] = useState(false);
+
     return (
-      <button className="text-white px-8 flex items-center hover:bg-white hover:bg-opacity-20">Burger Icon</button>
+      <MenuContext.Provider value={[isOpen, setOpen]}>
+        <button onClick={() => setOpen(!isOpen)} className="text-white px-8 flex items-center hover:bg-white hover:bg-opacity-20">Burger Icon</button>
+        {isOpen ? <MenuOverlay>{children}</MenuOverlay> : null}
+      </MenuContext.Provider>
+
     )
   }
   
+
+function MenuOverlay({children}) {
+
+  const [isOpen, setOpen] = useContext(MenuContext);
+
+  return (
+      <div onClick={() => setOpen(!isOpen)} className="z-10 bg-black bg-opacity-20 h-screen w-screen absolute flex items-stretch">
+        <div className="bg-black d-flex flex-col text-white w-60 pt-8 items-center">{children}</div>
+      </div>
+    )
+}
