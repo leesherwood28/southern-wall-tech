b02f146ba6b9323670cc0c337f286e49a9691fa9
lee sherwood <leesherwood28@googlemail.com>
2021-08-17 12:48:34 +0100

    Adding styles to contact button

--- components/contact-button.js
+++ components/contact-button.js
@@ -1,6 +1,6 @@
 
 export default function ContactButton() {
   return (
-    <button className="bg-pink-700 text-white h-full p-2">Contact</button>
+    <button className="bg-pink-700 text-white h-full p-2 hover:bg-pink-600">Contact</button>
   )
 }
