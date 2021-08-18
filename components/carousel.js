a4cd0e22895ee8259a091bc81027e6bbd7665b89
lee sherwood <leesherwood28@googlemail.com>
2021-08-18 12:45:00 +0100

    Getting images to work

--- components/carousel.js
+++ components/carousel.js
@@ -1,9 +1,44 @@
+import { useRef } from 'react'
+import { useSprings, animated } from '@react-spring/web'
+import useMeasure from 'react-use-measure'
+import { useDrag } from 'react-use-gesture'
+import clamp from 'lodash.clamp'
 import Image from 'next/image'
 
 export default function ImageCarousel({images}) {
+  const index = useRef(0);
+  const [ref, { width }] = useMeasure();
+  const [props, api] = useSprings(
+      images.length,
+      i => ({
+          x: i*width,
+          scale: width === 0 ? 0 : 1,
+          display: 'block',
+      }), 
+      [width]
+  )
+  const bind = useDrag(({ active, movement: [mx], direction: [xDir], distance, cancel }) => {
+    if (active && distance > width / 2) {
+      index.current = clamp(index.current + (xDir > 0 ? -1 : 1), 0, images.length - 1)
+      cancel()
+    }
+    api.start(i => {
+      if (i < index.current - 1 || i > index.current + 1) return { display: 'none' }
+      const x = (i - index.current) * width + (active ? mx : 0)
+      const scale = active ? 1 - distance / width / 2 : 1
+      return { x, scale, display: 'block' }
+    })
+  })
+
   return (
-    <div className="overflow-hidden flex items-center">
-        {images.map((im) => <CarouselImage key={im.href} image={im}></CarouselImage>)}
+    <div ref={ref} className="overflow-hidden flex items-center">
+        {props.map(({x, display, scale}, i) =>
+            <animated.div {...bind()} key={i} style={{ display, x }}>
+                 <animated.div style={{ scale}}>
+                     <CarouselImage image={images[i]}></CarouselImage>
+                 </animated.div>
+            </animated.div>
+        )}
     </div>
   )
 }
