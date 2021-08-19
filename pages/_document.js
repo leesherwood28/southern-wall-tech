da0ee35276fa04bcd35511f076bc79b18a134d22
lee sherwood <leesherwood28@googlemail.com>
2021-08-19 19:49:27 +0100

    Adding language english

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,23 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-    render() {
-      return (
-        <Html>
-          <Head>
-            <link rel="icon" href="/favicon.ico" />
-            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"></link>
-          </Head>
-          <body>
-            <Main />
-            <NextScript />
-          </body>
-        </Html>
-      )
-    }
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <link rel='icon' href='/favicon.ico' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap'
+            rel='stylesheet'
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
-  
-  export default MyDocument
+}
+
+export default MyDocument;
