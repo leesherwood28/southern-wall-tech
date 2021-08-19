203a21fbbabd3a188f1ae9a5e6e43e493b2bb5c9
lee sherwood <leesherwood28@googlemail.com>
2021-08-19 21:09:03 +0100

    Improving sidenav look and feel

--- components/side-nav-link.js
+++ components/side-nav-link.js
@@ -1,21 +1,27 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-export default function SideNavLink({href, text}) {
-    return (
+export default function SideNavLink({ href, text }) {
+  const router = useRouter();
+  return (
     <Link href={href}>
-      <a 
-        className="
+      <a
+        className={`
             text-white 
             px-8 
             py-4 
             flex 
             items-center 
             justify-center 
-            hover:bg-white 
-            hover:bg-opacity-20
-            active:bg-opacity-30">
-            {text}
-    </a>
-    </Link>)
-  }
-  
+            relative
+            ${router.pathname === href ? 'bg-white bg-opacity-20' : ''}
+            `}
+      >
+        {text}
+        {router.pathname === href ? (
+          <div className='w-2 bg-pink-700 absolute top-0 left-0 bottom-0'></div>
+        ) : null}
+      </a>
+    </Link>
+  );
+}
