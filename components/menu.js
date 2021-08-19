6894852ab15796fcbf59c8f5ad3e9b55169e8354
lee sherwood <leesherwood28@googlemail.com>
2021-08-19 20:24:00 +0100

    Adding menu button to gallery

--- components/menu.js
+++ components/menu.js
@@ -1,41 +1,59 @@
-import { createContext, useContext, useState } from "react"
-import { useSpring, animated } from '@react-spring/web'
-import {GiHamburgerMenu} from 'react-icons/gi'
+import { createContext, useContext, useState } from 'react';
+import { useSpring, animated } from '@react-spring/web';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const MenuContext = createContext(false);
 
-export default function Menu({children}) {
+export default function Menu({ children }) {
+  const [isOpen, setOpen] = useState(false);
 
-    const [isOpen, setOpen] = useState(false);
+  return (
+    <MenuContext.Provider value={[isOpen, setOpen]}>
+      <MenuButton></MenuButton>
+      <MenuOverlay>{children}</MenuOverlay>
+    </MenuContext.Provider>
+  );
+}
 
-    return (
-      <MenuContext.Provider value={[isOpen, setOpen]}>
-        <button 
-            onClick={() => setOpen(!isOpen)} 
-            className="text-white h-12 w-12 flex items-center justify-center hover:bg-white hover:bg-opacity-20">
-            <GiHamburgerMenu></GiHamburgerMenu>
-        </button>
-        <MenuOverlay>{children}</MenuOverlay>
-      </MenuContext.Provider>
-
-    )
-  }
-  
-
-function MenuOverlay({children}) {
-
+function MenuButton() {
   const [isOpen, setOpen] = useContext(MenuContext);
-  const styles = useSpring({ transform: isOpen ? 'translate3d(0%, 0, 0)' : 'translate3d(-100%,0,0)'});
 
-  
+  return (
+    <button
+      onClick={() => setOpen(!isOpen)}
+      className='text-white h-12 w-12 flex items-center justify-center hover:bg-white hover:bg-opacity-20'
+    >
+      <GiHamburgerMenu></GiHamburgerMenu>
+    </button>
+  );
+}
+
+function MenuOverlay({ children }) {
+  const [isOpen, setOpen] = useContext(MenuContext);
+  const styles = useSpring({
+    transform: isOpen ? 'translate3d(0%, 0, 0)' : 'translate3d(-100%,0,0)',
+  });
+
   return (
     <>
-    { !isOpen ? '' : (
-      <div onClick={() => setOpen(!isOpen)} className="z-10 bg-black bg-opacity-20 inset-0 absolute flex items-stretch">
-      </div>
-     )}
+      {!isOpen ? (
+        ''
+      ) : (
+        <div
+          onClick={() => setOpen(!isOpen)}
+          className='z-10 bg-black bg-opacity-20 inset-0 absolute flex items-stretch'
+        ></div>
+      )}
 
-      <animated.div style={styles} className="z-10 absolute h-screen left-0 bg-black d-flex flex-col text-white w-60 pt-20 items-center">{children}</animated.div>
+      <animated.div
+        style={styles}
+        className='z-10 absolute h-screen left-0 bg-black text-white w-60'
+      >
+        <div className='h-12 flex items-center'>
+          <MenuButton></MenuButton>
+        </div>
+        <div className='items-center flex flex-col pt-20'>{children}</div>
+      </animated.div>
     </>
-    )
+  );
 }
