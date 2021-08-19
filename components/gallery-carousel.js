2e87aec77bddbaabbd982acaae84542f49be1d80
lee sherwood <leesherwood28@googlemail.com>
2021-08-19 15:19:47 +0100

    Adding image gallery

--- components/gallery-carousel.js
+++ components/gallery-carousel.js
@@ -1,20 +1,10 @@
-/* eslint-disable react/jsx-key */
-import Image from 'next/image';
-import Carousel from './carousel';
+import ImageGallery from 'react-image-gallery';
 
 export default function GalleryCarousel() {
-  const images = [...Array(10).keys()].map((i) => `/gallery-${i}.jpg`);
+  const images1 = [...Array(10).keys()].map((i) => ({
+    original: `/gallery-${i}.jpg`,
+    thumbnail: `/gallery-${i}.jpg`,
+  }));
 
-  return (
-    <Carousel
-      className='h-96 max-w-xl sm:max-w-2xl sm:h-120 md:max-w-3xl md:h-140 w-full p-4'
-      userCanMove={true}
-      autoRotate={false}
-      showCounter={true}
-    >
-      {images.map((image) => (
-        <Image src={image} alt='blh' layout='fill' objectFit='contain'></Image>
-      ))}
-    </Carousel>
-  );
+  return <ImageGallery items={images1}></ImageGallery>;
 }
