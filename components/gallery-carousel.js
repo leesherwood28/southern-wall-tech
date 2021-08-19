7ad69cc9294b8d7c78492dfa715c8a8526e19fc1
lee sherwood <leesherwood28@googlemail.com>
2021-08-19 13:44:56 +0100

    Improving look of gallery

--- components/gallery-carousel.js
+++ components/gallery-carousel.js
@@ -5,32 +5,14 @@ import Carousel from './carousel';
 export default function GalleryCarousel() {
   const images = [...Array(10).keys()].map((i) => `/gallery-${i}.jpg`);
 
-  const adds = [
-    {
-      image: '/qa.svg',
-      alt: 'Image of quality assurance manager',
-      text: 'High Quality Plastering And Rendering',
-    },
-    {
-      image: '/contract.svg',
-      alt: 'Image of contract',
-      text: 'Proffesional Conduct',
-    },
-    {
-      image: '/houses.svg',
-      alt: 'Image of multiple buildings',
-      text: 'Over 20 Years Of Experience',
-    },
-  ];
-
   return (
     <Carousel
-      className='h-56 sm:h-60 md:h-72 lg:h-96 w-full p-4'
-      userCanMove={false}
-      autoRotate={true}
+      className='w-full max-w-xl h-96 p-4'
+      userCanMove={true}
+      autoRotate={false}
     >
       {images.map((image) => (
-        <Image src={image} alt='blh' width={334} height={464}></Image>
+        <Image src={image} alt='blh' layout='fill' objectFit='contain'></Image>
       ))}
     </Carousel>
   );
