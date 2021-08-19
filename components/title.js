85543dd7244621dfe45dbe4a527258363661be4f
lee sherwood <leesherwood28@googlemail.com>
2021-08-19 13:04:07 +0100

    Adding title

--- components/title.js
+++ components/title.js
@@ -1,10 +1,10 @@
 export default function Title() {
   return (
     <div className='flex flex-col items-center'>
-      <h1 className='text-3xl sm:text-4xl md:text-5xl font-light text-center mb-2'>
+      <h1 className='text-3xl sm:text-4xl md:text-5xl font-light text-center sm:mb-2'>
         Southern Wall Tech
       </h1>
-      <h3 className='text-xl italic font-light text-center'>
+      <h3 className='sm:text-xl italic font-light text-center'>
         Rendering and Plastering Solutions
       </h3>
     </div>
