8d5c683a83274546910741efc217a1ffb486f699
lee sherwood <leesherwood28@googlemail.com>
2021-08-19 15:22:00 +0100

    Improving carousel

--- components/gallery-carousel.js
+++ components/gallery-carousel.js
@@ -1,10 +1,10 @@
 import ImageGallery from 'react-image-gallery';
 
 export default function GalleryCarousel() {
-  const images1 = [...Array(10).keys()].map((i) => ({
+  const images = [...Array(10).keys()].map((i) => ({
     original: `/gallery-${i}.jpg`,
     thumbnail: `/gallery-${i}.jpg`,
   }));
 
-  return <ImageGallery items={images1}></ImageGallery>;
+  return <ImageGallery items={images}></ImageGallery>;
 }
