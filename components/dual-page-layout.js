2e4e5992c4f30f6f2a6b14da24037b4b6a9a8f5e
lee sherwood <leesherwood28@googlemail.com>
2021-08-19 19:30:00 +0100

    Using dual layout

--- components/dual-page-layout.js
+++ components/dual-page-layout.js
@@ -2,9 +2,9 @@ export default function DualPageLayout({ children }) {
   console.log(children);
   return (
     <div className='flex-1 flex flex-col md:flex-row py-2 sm:py-10 px-2 sm:px-12 items-stretch'>
-      {children.map((child) => (
+      {children.map((child, i) => (
         <div
-          key={child}
+          key={i}
           className='flex-1 overflow-hidden flex flex-col items-center justify-center'
         >
           {child}
