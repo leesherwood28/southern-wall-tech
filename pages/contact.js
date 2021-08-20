df3dc7fbfe0f3ca0d534cc049b4494a131fe6588
lee sherwood <leesherwood28@googlemail.com>
2021-08-20 17:00:13 +0100

    Initial add of contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Article from '../components/article';
+import ContactForm from '../components/contact-form';
 import DualPageLayout from '../components/dual-page-layout';
 
 export default function About() {
@@ -24,16 +25,7 @@ export default function About() {
         </div>
 
         <Article title='Contact'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-          <br />
-          <br />
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est
-          laborum.
+          <ContactForm></ContactForm>
         </Article>
       </DualPageLayout>
     </>
