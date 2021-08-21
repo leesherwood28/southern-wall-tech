393092a30822f3e8c959201c56deaa9a89fd4a69
lee sherwood <leesherwood28@googlemail.com>
2021-08-21 13:28:58 +0100

    Fixing flex issues

--- components/dual-page-layout.js
+++ components/dual-page-layout.js
@@ -2,7 +2,7 @@ export default function DualPageLayout({ children }) {
   return (
     <div className='flex-1 flex flex-col md:flex-row py-2 sm:py-10 px-2 sm:px-12 items-stretch'>
       {children.map((child, i) => (
-        <div key={i} className='flex-1 overflow-hidden'>
+        <div key={i} className='flex-1 min-w-0'>
           {child}
         </div>
       ))}
