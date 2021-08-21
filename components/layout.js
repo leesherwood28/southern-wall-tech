f3303e3efbead435a7f33491b5152a73ffdcfa5b
lee sherwood <leesherwood28@googlemail.com>
2021-08-21 10:59:15 +0100

    Adding state

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,19 @@
+import { useEffect, useState } from 'react';
 import Header from './header';
 
 export default function Layout({ children }) {
+  const [state, setState] = useState({ prev: null, next: null });
+  useEffect(() => {
+    setState((s) => ({
+      prev: s.next,
+      next: children,
+    }));
+  }, [children]);
   return (
     <div className='h-screen w-screen flex flex-col items-stretch'>
       <Header></Header>
       <main className='overflow-y-auto flex-grow flex flex-col items-stretch'>
-        {children}
+        {state.next}
       </main>
     </div>
   );
