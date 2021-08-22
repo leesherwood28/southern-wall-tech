5f4b8cb5f2ced90b2c74de9be3b1f2d68676b482
lee sherwood <leesherwood28@googlemail.com>
2021-08-22 14:18:33 +0100

    GOT ROUTE TRANSITIONS WORKING

--- components/layout.js
+++ components/layout.js
@@ -1,50 +1,29 @@
-import { animated, useSprings } from '@react-spring/web';
-import { useLayoutEffect, useState } from 'react';
+import { animated, useTransition } from '@react-spring/web';
 import Header from './header';
 
 export default function Layout({ children }) {
-  const [state, setState] = useState([]);
-
-  const [childSprings, springApi] = useSprings(state.length, (i) => ({
-    opacity: 0,
-  }));
-
-  useLayoutEffect(() => {
-    setState((s) => {
-      const state = [...s];
-      state.push({ childEl: children, key: Math.random() });
-      return state.slice(Math.max(0, state.length - 2));
-    });
-  }, [children, springApi]);
-
-  useLayoutEffect(() => {
-    springApi.start((i) => {
-      const isSingle = state.length === 1;
-      const fadedOut = { opacity: 0 };
-      const displayed = { opacity: 1 };
-      if (isSingle) {
-        return { from: fadedOut, to: displayed };
-      }
-      return {
-        from: i === 0 ? displayed : fadedOut,
-        to: i === 0 ? fadedOut : displayed,
-      };
-    });
-  }, [state, springApi]);
+  const transitions = useTransition(children, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: {
+      opacity: 0,
+    },
+  });
 
   return (
     <div className='h-screen w-screen flex flex-col items-stretch'>
       <Header></Header>
       <main className='flex-grow relative'>
-        {state.map((child, i) => (
-          <animated.div
-            className='overflow-y-auto flex flex-col items-stretch absolute inset-0'
-            key={child.key}
-            style={childSprings[i]}
-          >
-            {child.childEl}
-          </animated.div>
-        ))}
+        {transitions((props, item) => {
+          return (
+            <animated.div
+              className='overflow-y-auto flex flex-col items-stretch absolute inset-0'
+              style={props}
+            >
+              {item}
+            </animated.div>
+          );
+        })}
       </main>
     </div>
   );
