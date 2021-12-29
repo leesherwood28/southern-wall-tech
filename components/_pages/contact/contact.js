eee51412e977e640d4fa69080d1277e52e3516fe
Lee Sherwood <leesherwood28@googlemail.com>
2021-12-29 22:10:08 +0000

    Improving site

--- components/_pages/contact/contact.js
+++ components/_pages/contact/contact.js
@@ -5,21 +5,19 @@ import ContactForm from './contact-form';
 
 export default function ContactPage() {
   return (
-    <div id='contact' className='min-h-screen flex items-center'>
-      <DualPageLayout>
-        <div className='relative h-full w-full'>
-          <Image
-            src='/message.svg'
-            alt='Picture of envelope'
-            layout='fill'
-            objectFit='contain'
-          ></Image>
-        </div>
+    <div id='contact' className='min-h-screen grid grid-cols-1 lg:grid-cols-2'>
+      <div className='relative'>
+        <Image
+          src='/message.svg'
+          alt='Picture of envelope'
+          layout='fill'
+          objectFit='contain'
+        ></Image>
+      </div>
 
-        <Article title='Contact'>
-          <ContactForm className='w-full'></ContactForm>
-        </Article>
-      </DualPageLayout>
+      <Article title='Contact'>
+        <ContactForm className='w-full'></ContactForm>
+      </Article>
     </div>
   );
 }
--- components/dual-page-layout.js
+++ components/dual-page-layout.js
@@ -1,6 +1,6 @@
 export default function DualPageLayout({ children }) {
   return (
-    <div className='flex-1 flex flex-col md:flex-row py-2 sm:py-10 px-2 sm:px-12 items-stretch'>
+    <div className='flex-1 flex flex-col lg:flex-row lg:items-center py-2 sm:py-10 px-2 sm:px-12 items-stretch'>
       {children.map((child, i) => (
         <div key={i} className='flex-1 min-w-0'>
           {child}
