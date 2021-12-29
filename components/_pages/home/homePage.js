e930ae6be16680351f0ae83be1d86f5bcc67cbf6
Lee Sherwood <leesherwood28@googlemail.com>
2021-12-29 22:22:04 +0000

    Setting up logo

--- components/_pages/home/homePage.js
+++ components/_pages/home/homePage.js
@@ -1,6 +1,7 @@
 import AdCarousel from './ad-carousel';
 import Summary from './summary';
 import Title from './title';
+import Image from 'next/image';
 
 export function HomePage() {
   return (
@@ -15,8 +16,13 @@ export function HomePage() {
         <div className='lg:w-4/5 xl:w-3/5 flex flex-col items-center'>
           <section></section>
 
-          <section>
-            <Title></Title>
+          <section className='md:px-20'>
+            <Image
+              alt='Southern Wall Tech Title'
+              src='/logo-full.png'
+              width={3400}
+              height={1374}
+            ></Image>
           </section>
 
           <section className='mt-4 px-4 w-full sm:w-3/5'>
