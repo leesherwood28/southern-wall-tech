8c58396ba4c26e594533c727bc15c24d7d5e8f39
Lee Sherwood <leesherwood28@googlemail.com>
2022-01-02 16:37:01 +0000

    Setting up scroll reveal

--- components/_pages/contact/contact.js
+++ components/_pages/contact/contact.js
@@ -1,22 +1,27 @@
 import Image from 'next/image';
 import Article from '../../shared/article';
+import { ScrollReveal } from '../../shared/scroll-reaveal';
 import ContactForm from './contact-form';
 
 export function ContactPage() {
   return (
     <div id='contact' className='min-h-screen grid grid-cols-1 lg:grid-cols-2'>
       <div className='relative'>
-        <Image
-          src='/message.svg'
-          alt='Picture of envelope'
-          layout='fill'
-          objectFit='contain'
-        ></Image>
+        <ScrollReveal>
+          <Image
+            src='/message.svg'
+            alt='Picture of envelope'
+            layout='fill'
+            objectFit='contain'
+          ></Image>
+        </ScrollReveal>
       </div>
 
-      <Article title='Contact'>
-        <ContactForm className='w-full'></ContactForm>
-      </Article>
+      <ScrollReveal>
+        <Article title='Contact'>
+          <ContactForm className='w-full'></ContactForm>
+        </Article>
+      </ScrollReveal>
     </div>
   );
 }
