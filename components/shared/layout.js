409dc7d55f6d804c32b78e9d7dd181b73479fb87
Lee Sherwood <leesherwood28@googlemail.com>
2022-01-02 19:37:20 +0000

    Adding overflow hidden

--- components/shared/layout.js
+++ components/shared/layout.js
@@ -2,7 +2,7 @@ import Header from './header';
 
 export default function Layout({ children }) {
   return (
-    <div className='h-screen w-screen flex flex-col items-stretch'>
+    <div className='h-screen w-screen flex flex-col items-stretch overflow-hidden'>
       <Header></Header>
       <main className='flex-1 overflow-y-auto overflow-x-hidden scroll-smooth'>
         {children}
