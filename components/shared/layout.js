4bf03a38598d0108a857d69c2c02d80e8aa75fe7
Lee Sherwood <leesherwood28@googlemail.com>
2022-01-02 20:05:58 +0000

    Fixing layout

--- components/shared/layout.js
+++ components/shared/layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <div className='h-screen w-screen flex flex-col items-stretch overflow-hidden'>
       <Header></Header>
-      <main className='flex-1 overflow-y-auto overflow-x-hidden scroll-smooth'>
+      <main className='flex-1 overflow-y-auto overflow-x-hidden scroll-smooth mt-12'>
         {children}
       </main>
     </div>
