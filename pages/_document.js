d46501ab54118e1e936a2a2f5d15450e244e6a56
Lee Sherwood <leesherwood28@googlemail.com>
2022-01-05 17:54:38 +0000

    Adding links

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <link rel='icon' href='/logo.png' />
+          <meta name='robots' content='all' />
           <link
             href='https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap'
             rel='stylesheet'
