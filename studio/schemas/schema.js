cd3c71a8cad919af19d771dbd41b5749427a65f5
Lee Sherwood <leesherwood28@googlemail.com>
2022-05-02 17:52:19 +0100

    Setting up shcema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -5,10 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // We import object and document schemas
-import blockContent from './blockContent';
-import category from './category';
 import service from './service';
-import author from './author';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -20,10 +17,5 @@ export default createSchema({
     // The following are document types which will appear
     // in the studio.
     service,
-    author,
-    category,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
   ]),
 });
