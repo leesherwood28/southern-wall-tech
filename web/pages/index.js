6e1fe3de5b28b411d5c89b01778ec5337531a1d4
Lee Sherwood <leesherwood28@googlemail.com>
2022-05-02 18:54:14 +0100

    setting up nextjs

--- web/pages/index.js
+++ web/pages/index.js
@@ -4,6 +4,16 @@ import { HomePage } from '../components/_pages/home/homePage';
 import { PlasteringPage } from '../components/_pages/plastering/plastering-page';
 import { RenderingPage } from '../components/_pages/rendering/rendering-page';
 
+export async function getStaticProps() {
+  const intro = await client.fetch(`*[_type == "intro"]`);
+  return {
+    props: {
+      intro,
+    },
+    revalidate: 1000, // In seconds
+  };
+}
+
 export default function Index() {
   return (
     <>
